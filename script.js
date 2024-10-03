9fca74d49106ed864183479c4c218ccc0eae4fb1
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2024-10-03 16:19:45 -0300

    Voltar a usar o carrossel slick

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        slidesToShow: 3, 
+        slidesToScroll: 1, 
+        autoplay: true, 
+        autoplaySpeed: 12000, 
+        dots: false,  
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true
+                }
+            }
+        ]
+    });
+});
+
+/* -- carrossel inhouse
 window.onload = () => {
   // (adapted from https://stackoverflow.com/questions/13435604/getting-an-elements-inner-height)
   function getInnerSize(elm) {
@@ -204,6 +243,7 @@ window.onload = () => {
 
   carousel.onclick = click;
 };
+*/
 
 let elementoAnterior = null;
 let originalContent = document.getElementById("content").innerHTML;
